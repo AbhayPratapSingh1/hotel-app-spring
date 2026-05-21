1688a635022ff54922233b57a8e0316ab0ce7ad1
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-05-21 11:24:26 +0530

    Adds basic file/folder structuring

--- src/main/java/com/hotel/app/HotelApp.java
+++ src/main/java/com/hotel/app/HotelApp.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.hotel.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class HotelApp {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HotelApp.class, args);
 	}
-
 }
