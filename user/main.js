0cfde2cec464aa89f89e78191c98ff96742da158
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-05-22 14:45:47 +0530

    adds code for the login and signin in js

--- user/main.js
+++ user/main.js
@@ -1,7 +1,12 @@
 import { createApp } from "./src/app.js";
 
-const main = () => {
-  const app = createApp();
+
+
+const main = async () => {
+  const app = await createApp();
+
+
+
 
   Deno.serve({ port: 8080 }, app.fetch);
 }
