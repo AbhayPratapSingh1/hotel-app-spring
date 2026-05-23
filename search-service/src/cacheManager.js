f1bcda108c65bc0f2ee9624c640ef1d3b161a7d8
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-05-23 11:23:00 +0530

    Adds redis for caching

---
+++ search-service/src/cacheManager.js
@@ -0,0 +1,45 @@
+const EXPIRY_TIME = 30;
+
+export class CacheManager {
+  constructor(client) {
+    this.client = client
+  }
+
+  async setHotelsCache(id, hotelDetails) {
+
+    console.log(id, hotelDetails);
+
+    const key = `hotel-set-id:${id}`
+
+    await this.client.set(key, JSON.stringify(hotelDetails), { EX: EXPIRY_TIME })
+
+    console.log("Cache Stored", id);
+  }
+
+  async getHotelCache(id) {
+    const key = `hotel-set-id:${id}`
+
+
+    const store = await this.client.get(key);
+    const hotelDetail = JSON.parse(store);
+
+    if (hotelDetail === null) {
+      console.log("\t\tCache Miss");
+      return null;
+    }
+    console.log("\t\tCache Hit");
+
+    return hotelDetail
+  }
+
+
+  async invalidate(id) {
+    const key = `hotel-set-id:${id}`
+
+    console.log("clearing cache");
+    await this.client.del(key);
+
+  }
+}
+
+
